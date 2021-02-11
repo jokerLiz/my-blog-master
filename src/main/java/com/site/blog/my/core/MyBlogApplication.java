563dc9440f0ac1b8e0aa5ad854404989278dd643
lizhao <li_zhao@uxsino.com>
2021-02-12 01:30:56 +0800

    #005 更改数据源为druid连接池,继承redis缓存 引入druid的依赖,在主配置类中更改数据源类型为druid; 引入集成redis缓存的依赖,在主配置类中配置redis的参数,在主启动类上开启缓存注解,配置完成,具体缓存的内内容在具体的业务中实现

--- src/main/java/com/site/blog/my/core/MyBlogApplication.java
+++ src/main/java/com/site/blog/my/core/MyBlogApplication.java
@@ -2,9 +2,11 @@ package com.site.blog.my.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
+@EnableCaching            // 开启spring boot缓存
 public class MyBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyBlogApplication.class, args);
