dbf46b9036f0d0c69c3efc16cca3df3cb868a691
lizhao <li_zhao@uxsino.com>
2021-02-21 01:03:20 +0800

    009 使用Lombok注解@Data简化代码

--- src/main/java/com/site/blog/my/core/controller/vo/SimpleBlogListVO.java
+++ src/main/java/com/site/blog/my/core/controller/vo/SimpleBlogListVO.java
@@ -1,26 +1,15 @@
 package com.site.blog.my.core.controller.vo;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class SimpleBlogListVO implements Serializable {
 
     private Long blogId;
 
     private String blogTitle;
 
-    public Long getBlogId() {
-        return blogId;
-    }
 
-    public void setBlogId(Long blogId) {
-        this.blogId = blogId;
-    }
-
-    public String getBlogTitle() {
-        return blogTitle;
-    }
-
-    public void setBlogTitle(String blogTitle) {
-        this.blogTitle = blogTitle;
-    }
 }
